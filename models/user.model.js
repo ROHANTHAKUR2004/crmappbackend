0d1e6bd7f8c17bd973ef7293f25dd8bd9140fcb0
rohan <rohanthakur89768@gmail.com>
2024-05-25 08:39:59 +0530

    fix bugs

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,8 @@
-const bcrypt = require("bcrypt");
+
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 const {Schema} = mongoose;
+
 
 const userSchema = new Schema({
     name:{
